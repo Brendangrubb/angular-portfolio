269537a7d2cea29bc2e295d2d25850564499f04f
Brendan Grubb <brendangrubb@gmail.com>
2017-08-04 13:06:04 -0700

    add style and animation to work component squares

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-work',
@@ -10,6 +12,23 @@ export class WorkComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('#contact').toggleClass('contact');
   }
 
+  showMoreInfoOne() {
+    $('.text-body-one').slideToggle('display-none');
+    $('.more-info-one').slideToggle('display-none');
+  }
+  showMoreInfoTwo() {
+    $('.text-body-two').slideToggle('display-none');
+    $('.more-info-two').slideToggle('display-none');
+  }
+  showMoreInfoThree() {
+    $('.text-body-three').slideToggle('display-none');
+    $('.more-info-three').slideToggle('display-none');
+  }
+  showMoreInfoFour() {
+    $('.text-body-four').slideToggle('display-none');
+    $('.more-info-four').slideToggle('display-none');
+  }
 }
